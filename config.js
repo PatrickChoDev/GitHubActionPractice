fae375fafc87880f3b617ee878c55f90b0fad447
Thanapat Chotipun <devpatrick.cho@gmail.com>
2025-04-06 23:17:31 +0700

    fix crypto

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const crypto = require("crypto");
 
 const crypto_orig_createHash = crypto.createHash;
 crypto.createHash = (algorithm) =>
-  crypto.createHash(algorithm == "md4" ? "sha256" : algorithm);
+  crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
